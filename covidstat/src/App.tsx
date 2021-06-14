cf8711b875ba933d8004e65190048fc8ae8df717
peterhencz <peter.hencz@gmail.com>
2021-06-14 17:51:10 +0200

    feat: change chart type on buttonclick

--- covidstat/src/App.tsx
+++ covidstat/src/App.tsx
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Navbar, Button, Chart } from "./components";
 import "./App.scss";
 
 function App() {
+  const [chartType, setChartType] = useState<string>("bar");
+
   return (
     <div className="App">
       <Navbar>
         <div>
-          <Button type="button" additionalStyles="btn-primary">
+          <Button
+            type="button"
+            additionalStyles="btn-primary"
+            onClick={() => setChartType("pie")}>
             chart 1
-          </Button>
-          <Button type="button" additionalStyles="btn-primary">
+          </Button>   
+          <Button
+            type="button"
+            additionalStyles="btn-primary"
+            onClick={() => setChartType("bar")}>
             chart 2
           </Button>
-          <Button type="button" additionalStyles="btn-primary">
+          <Button
+            type="button"
+            additionalStyles="btn-primary"
+            onClick={() => setChartType("radial")}>
             chart 3
           </Button>
         </div>
       </Navbar>
 
       <div className="container-fluid d-flex vh-100 w-100 justify-content-center align-items-center">
-        <Chart type="radial" />
+        <Chart type={chartType} />
       </div>
     </div>
   );
--- covidstat/src/components/Chart/types.ts
+++ covidstat/src/components/Chart/types.ts
@@ -1,5 +1,5 @@
 import React from "react";
 
 export interface ChartProps {
-  type: "bar" | "pie" | "radial";
+  type: string;
 }
